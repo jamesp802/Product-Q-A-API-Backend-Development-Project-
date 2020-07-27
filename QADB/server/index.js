3170dab19d96fc65e5115f6b09649d3a05e6d914
EC2 Default User <ec2-user@ip-172-31-47-220.us-east-2.compute.internal>
2020-07-27 22:42:24 +0000

    MySQL changed to Pool Connection

--- QADB/server/index.js
+++ QADB/server/index.js
@@ -6,8 +6,19 @@ const app = express();
 const routers = require('./routes');
 
 app.use(cors());
-app.use(bodyParser())
+app.use(bodyParser());
 app.use('/sql/', routers.routerSQL);
 
+app.get('/loaderio-a91a4b03da2639ccb5c57683de7c1ac3', (req, res) => {
+  res.send('loaderio-a91a4b03da2639ccb5c57683de7c1ac3');
+})
+
+
 const port = process.env.PORT || 1337;
-app.listen(port, () => console.log(`service is running on port ${port}`));
+app.listen(port, ((data, err) => {
+ if (err) {
+ console.log(err);
+ } else {
+ console.log(`service is running on port ${port}`);
+ }
+}));
